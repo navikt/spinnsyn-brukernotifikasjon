ae1c962c1966b48279fb26e5b7bfb60ec8c0f4e1
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-11-24 10:12:45 +0100

    Logger oslo tid

--- src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
@@ -4,13 +4,18 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.BrukernotifikasjonService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @Component
 class BrukernotifikasjonJob(
     val leaderElection: LeaderElection,
     val brukernotifikasjonService: BrukernotifikasjonService
 ) {
-    val log = logger()
+    private val log = logger()
+    private val osloZone: ZoneId = ZoneId.of("Europe/Oslo")
+    private val tidspunkt: ZonedDateTime = LocalDateTime.of(2021, 11, 24, 12, 0).atZone(osloZone)
 
     @Scheduled(
         initialDelay = 1000L * 60 * 2,
@@ -19,6 +24,8 @@ class BrukernotifikasjonJob(
     fun run() {
         if (leaderElection.isLeader()) {
             brukernotifikasjonService.cronJob()
+
+            log.info("Tidspunktet er: $tidspunkt")
         } else {
             log.info("Kjører ikke cronjob siden denne podden ikke er leader")
         }
