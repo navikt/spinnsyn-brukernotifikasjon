00642ccdab0e69ee891a74218d00bb4530ffe4fc
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-03-08 10:13:33 +0100

    Timeunit

--- src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
@@ -4,6 +4,7 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.BrukernotifikasjonService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class BrukernotifikasjonJob(
@@ -13,8 +14,9 @@ class BrukernotifikasjonJob(
     private val log = logger()
 
     @Scheduled(
-        initialDelay = 1000L * 60 * 2,
-        fixedDelay = 1000L * 60 * 10,
+        initialDelay = 2,
+        fixedDelay = 10,
+        timeUnit = TimeUnit.MINUTES
     )
     fun run() {
         if (leaderElection.isLeader()) {
