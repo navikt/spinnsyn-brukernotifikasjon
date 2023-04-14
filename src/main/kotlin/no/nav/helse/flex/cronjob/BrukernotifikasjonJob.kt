7a6a1973ea881dbc85fe2d41832835fac22eb028
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2023-04-14 09:16:44 +0200

    Revert "Skaler til 1 med kafka, ikke cronjob (#121)" (#122)
    This reverts commit 74c15b5774213aa324cb3e7c2b1cc13f99274610.

--- src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
@@ -2,7 +2,9 @@ package no.nav.helse.flex.cronjob
 
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.BrukernotifikasjonService
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class BrukernotifikasjonJob(
@@ -11,11 +13,11 @@ class BrukernotifikasjonJob(
 ) {
     private val log = logger()
 
-/*    @Scheduled(
+    @Scheduled(
         initialDelay = 2,
         fixedDelay = 10,
         timeUnit = TimeUnit.MINUTES
-    )*/
+    )
     fun run() {
         if (leaderElection.isLeader()) {
             brukernotifikasjonService.cronJob()
